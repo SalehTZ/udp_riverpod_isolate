e621039889ebf5ba62a3da7b997cd1f44c1c580a
SalehTZ <31685655+SalehTZ@users.noreply.github.com>
2024-08-13 15:26:27 +0330

    Initialized hive database.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:udp_ws_riverpod_isolate/domain/models/messages_model.dart';
 
+import 'application/providers/device_id_provider.dart';
 import 'presentation/screen/home_screen.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Hive.registerAdapter(MessageModelAdapter());
+  await Hive.openBox<MessageModel>('messagesBox');
 
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.read(getDeviceIdProvider);
+
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
